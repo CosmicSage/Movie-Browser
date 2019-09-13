94ec43bfce7bfc0dbd933e000d0400cfaf6239fb
CosmicSage <prabhakarmhw@gmail.com>
2019-09-14 00:11:49 +0530

    namespace contr

--- SettingsPage.js
+++ SettingsPage.js
@@ -4,7 +4,7 @@ import {Switch, Text, View, StyleSheet} from "react-native";
 class SettingsPage extends React.Component {
   state = {
     bool : false,
-    randomDanOpo : "df"
+    randomDanOpo : "dfd"
   }
   Change = () => {
     this.setState(prevState => ({
